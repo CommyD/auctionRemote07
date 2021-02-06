74059659a8deef867f9aeb7be8adf7f58337a1fe
CosminD <cosmindanaita@gmail.com>
2021-02-06 14:19:45 +0200

    added registration page

--- src/main/java/com/sda/auction/controller/HomeController.java
+++ src/main/java/com/sda/auction/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.sda.auction.controller;
 
+import com.sda.auction.dto.UserDto;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +13,18 @@ public class HomeController {
     @GetMapping("/home")
     public String getHomePage(){
         return "home";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("userDto", new UserDto());
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegisterPage(UserDto userDto){
+        System.out.println("S-a apelat aceasta metoda cu " + userDto);
+        return "register";
     }
 }
